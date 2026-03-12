7275b949295b7c49746e1a5baa121b83a82c84d6
VinodiNikeshaniKasthuri <vinodinikeshani@gmail.com>
2026-03-12 17:24:56 +0530

    update main.ts

--- book-app/src/main.ts
+++ book-app/src/main.ts
@@ -1,6 +1,7 @@
+import { provideHttpClient } from '@angular/common/http';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { App } from './app/app';
+import { AppComponent } from './app/app.component';
 
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [provideHttpClient()]
+}).catch(err => console.error(err));
