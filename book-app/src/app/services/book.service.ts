2f2dd7f0b7dcadced9d78231b062383a52f10e38
VinodiNikeshaniKasthuri <vinodinikeshani@gmail.com>
2026-03-12 16:53:41 +0530

    feat : create book.service.ts

---
+++ book-app/src/app/services/book.service.ts
@@ -0,0 +1,29 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Book } from '../models/book';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BookService {
+  apiUrl = "http://localhost:5062/api/books"; // backend API URL
+
+  constructor(private http: HttpClient) { }
+
+  getBooks(): Observable<Book[]> {
+    return this.http.get<Book[]>(this.apiUrl);
+  }
+
+  addBook(book: Book): Observable<Book> {
+    return this.http.post<Book>(this.apiUrl, book);
+  }
+
+  updateBook(id: number, book: Book): Observable<any> {
+    return this.http.put(`${this.apiUrl}/${id}`, book);
+  }
+
+  deleteBook(id: number): Observable<any> {
+    return this.http.delete(`${this.apiUrl}/${id}`);
+  }
+}
